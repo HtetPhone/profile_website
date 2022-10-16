91fa1ba75346c7fb5d23d0b54aa81a6fb03a8db8
Htet Phone <senseihtetphone@gmail.com>
2022-10-16 22:14:28 +0630

    profile website to drago

--- js/app.js
+++ js/app.js
@@ -1,94 +1,85 @@
-//loader
-$(window).on("load", function () {
-  $(".loader-box").fadeOut(500);
+// slick
+$('.pricing-carousel').slick({
+    infinite: true,
+    // mobileFast: true,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    responsive: [
+  {
+    breakpoint: 1000,
+    settings: {
+      slidesToShow: 3,
+    }
+  },
+  {
+    breakpoint: 800,
+    settings: {
+      slidesToShow: 2,
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+    }
+  }]
+});
+
+// to deal with nav on sm 
+$(document).ready(function(){
+    $('.navbar-toggler').click(function() {
+        $('.header').toggleClass('white-header')
+    })
+})
+$('.nav-link').click(() => {
+  //  $('.navbar-toggler').attr('aria-expanded','false');
+    $('.navbar-collapse').removeClass('show');
+})
+// to deal with nav on sm 
+
+
+//nav on scroll
+window.addEventListener("scroll", () => {
+    let header = document.querySelector('#header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+    $('.header').removeClass('white-header')
+
 })
 
-//slick
-$(document).ready(function () {
-    $('.mulitple-items').slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      dots: true,
-      arrows: false,
-      autoplay: true,
-      responsive: [
-        {
-          breakpoint: 820,
-          settings:{
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 576,
-          settings:{
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-      
-    });
+// one page nav 
+$(document).ready(function() {
+$('.navbar-nav').onePageNav({
+    currentClass : 'active',
 });
-    
-//wow
-wow = new WOW(
+});
+
+//wow js
+var wow = new WOW(
   {
-  boxClass:     'wow',      // default
-  animateClass: 'animated', // default
-  offset:       4,          // default
-  mobile:       true,       // default
-  live:         true,        // default
-  delay:      0.5
-  
-}
-)
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    scrollContainer: null // optional scroll container selector, otherwise use window
+  }
+);
 wow.init();
 
-//waypoint
-function onScroll() {
-  $("header").removeClass("nav-section-scroll");
-  $("header").addClass("nav-section-scroll");
-}
 
-var waypoints = $('#about').waypoint({
-  handler: function() {
-    onScroll();
-    $(".nav-link").removeClass("active");
-    $("a[href='#about']").addClass('active');
-  },
-})
-var waypoints = $('#services').waypoint({
-  handler: function() {
-    onScroll();
-    $(".nav-link").removeClass("active");
-    $("a[href='#services']").addClass('active');
-  },
-  // offset: 10
-})
-var waypoints = $('#pricing').waypoint({
-  handler: function() {
-    onScroll();
-    $(".nav-link").removeClass("active");
-    $("a[href='#pricing']").addClass('active');
-  },
-  // offset: "100"
-})
-var waypoints = $('#contact').waypoint({
-  handler: function() {
-    onScroll();
-    $(".nav-link").removeClass("active");
-    $("a[href='#contact']").addClass('active');
-  },
-  // offset: "100"
-})
-var waypoints = $('#home').waypoint({
-  handler: function() {
-    console.log('home');
-    $("header").removeClass("nav-section-scroll");
-    $(".nav-link").removeClass("active");
-    $("a[href='#home']").addClass('active');
-  },
-  offset: "-20%"
-})
+//animation on scroll
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry);
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('animate__slideInLeft');
+//     }else {
+//       entry.target.classList.remove('animate__slideInLeft')
+//     }
+//   })
+// });
+
+// const hiddenEl = document.querySelectorAll('.element');
+// hiddenEl.forEach( (el) => observer.observe(el));
